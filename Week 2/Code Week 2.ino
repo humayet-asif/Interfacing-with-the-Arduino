1c070fd41651955d0dc4bfeb3336ec1c9480fd7d
Humayet Asif <42934258+humayet-asif@users.noreply.github.com>
2020-04-30 21:23:26 +0600

    Add files via upload

---
+++ Week 2/Code Week 2.ino
@@ -0,0 +1,21 @@
+int ledPin = 7;
+int ldrPin = A0;
+
+void setup()
+{
+  pinMode(ledPin,OUTPUT);
+  pinMode(ldrPin,INPUT);
+}
+void loop()
+{
+int ldrStatus = analogRead(ldrPin);
+if(ldrStatus <= 200)
+{
+  digitalWrite(ledPin,LOW);
+}
+else
+{
+  digitalWrite(ledPin,HIGH);
+}
+}
+
